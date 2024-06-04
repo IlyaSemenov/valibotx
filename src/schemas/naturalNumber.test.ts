e7b9d0cc5f81e89d1ad6bb3e04fa95fa4f54eac3
Ilya Semenov <ilya@semenov.co>
2024-06-04 10:56:34 +0700

    Use the new v.config for naturalNumber

--- src/schemas/naturalNumber.test.ts
+++ src/schemas/naturalNumber.test.ts
@@ -7,9 +7,12 @@ test("main", () => {
   for (const value of [1, 2, 123]) {
     expect(v.parse(naturalNumber(), value)).toBe(value)
   }
-  for (const value of [0, -1, -5]) {
+  for (const value of [0, -1, -5, -1.5]) {
     expect(() => v.parse(naturalNumber(), value)).toThrow("Natural value expected")
   }
   expect(() => v.parse(naturalNumber(), 123.45)).toThrow("Natural value expected")
   expect(() => v.parse(naturalNumber("Please enter valid ID."), 123.45)).toThrow("Please enter valid ID.")
+
+  // Test that there is only one issue.
+  expect(v.flatten(v.safeParse(naturalNumber(), -1.5).issues!)).toEqual({ root: ["Natural value expected."] })
 })
--- src/schemas/naturalNumber.ts
+++ src/schemas/naturalNumber.ts
@@ -6,6 +6,6 @@ import * as v from "valibot"
  * @returns A natural number schema.
  */
 export function naturalNumber(message?: string) {
-  // can't just use v.integer() and v.minValue(1) where, see https://github.com/fabian-hiller/valibot/issues/618
-  return v.pipe(v.number(message), v.custom(x => typeof x === "number" && Number.isInteger(x) && x > 0, message ?? "Natural value expected."))
+  const naturalMessage = message ?? "Natural value expected."
+  return v.config(v.pipe(v.number(message), v.integer(naturalMessage), v.minValue(1, naturalMessage)), { abortEarly: true })
 }
